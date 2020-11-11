02b846247048a272534c45b98fb8019e728563b3
snus-spirit <macum001@mail.ru>
2020-11-11 14:53:13 +0300

    Add

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -1,3 +1,30 @@
 public class Tree {
 
+    void contains(int key) {
+        //TODO
+    }
+
+    void splay() {}
+
+    void zig(){}
+
+    void zigzag(){}
+
+    void zigzig(){}
+
+    void add(int key) {
+        //TODO
+    }
+
+    void remove(int key) {
+        //TODO
+    }
+
+    void split() {
+        //TODO
+    }
+
+    void merge() {
+        //TODO
+    }
 }
